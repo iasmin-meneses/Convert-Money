df02ddb01394c960136a193dd3703a6551a38ead
Iasmin Meneses <iasmin.meneses@gmail.com>
2023-01-16 14:29:58 -0300

    adc api convert values

--- scripts.js
+++ scripts.js
@@ -4,15 +4,17 @@ const button = document.getElementById('convert-button')
 
 const select = document.getElementById("currency-select")
 
-const dolar = 5.38
-const euro = 5.66
-const bitcoin = 90248.43
 
-const convertValues = () => {
+const convertValues = async () => {
     const inputReal = document.getElementById('input-real').value
-
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }).format(inputReal)
@@ -29,7 +31,7 @@ const convertValues = () => {
     }
 
     if (select.value === "Bitcoin") {
-        currencyValueText.innerHTML = (inputReal / bitcoin).toFixed(2)
+        currencyValueText.innerHTML = (inputReal / (bitcoin*1000)).toFixed(2)
     }
 }
 
